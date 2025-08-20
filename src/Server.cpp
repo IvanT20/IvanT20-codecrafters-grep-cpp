ab952174e562adf79e4ed9f408290e28416a329d
Ivan Tat <ivtat3048@gmail.com>
2025-08-20 02:28:03 -0400

    added negative character groups

--- src/Server.cpp
+++ src/Server.cpp
@@ -14,13 +14,14 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
     {
         return input_line.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_") != std::string::npos;
     }
-    else if (pattern[0] == '[' && pattern[pattern.length() - 1] == ']')
+    else if (pattern.front() == '[' && pattern.back() == ']')
     {   
-        if (pattern.length() > 2)
+        if (pattern[1] == '^')
         {
-            return input_line.find_first_of(pattern.substr(1, pattern.size() - 2)) != std::string::npos;
+            return input_line.find_first_not_of(pattern.substr(2, pattern.size() - 3)) != std::string::npos;
+            
         }
-        throw std::runtime_error("Unhandled pattern " + pattern);
+        return input_line.find_first_of(pattern.substr(1, pattern.size() - 2)) != std::string::npos;
     }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
